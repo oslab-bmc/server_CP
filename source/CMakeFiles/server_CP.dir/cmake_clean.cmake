c4fbd9ac6ad15b7142325ec3cd1d63fa91898299
hwangdaewoong <mwgjulie@gmail.com>
2023-07-22 22:57:37 +0900

    add_source_code

---
+++ source/CMakeFiles/server_CP.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server_CP.dir/server_CP.o"
+  "CMakeFiles/server_CP.dir/server_CP.o.d"
+  "server_CP"
+  "server_CP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server_CP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
